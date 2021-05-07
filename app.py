d4414e31699a779677a1967b7c726447b01cbeba
Arturo Galvan <agalvanalarcon@gmail.com>
2021-05-07 14:12:25 -0700

    modified app

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-import time
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-  return 'Goodbye World ):'
+def index():
+    file = 'count.txt'
+    try:
+        file = open('count.txt', 'r')
+        count = int(file.read())
+    except Exception as inst:
+        print(inst)
+        count = 0
+    finally:
+        file.close()
+
+    count += 1
+
+    try:
+        file = open('count.txt', 'w')
+        file.write(str(count))
+    except Exception as inst:
+        logging.warning('Exception occurred in writefile: %s', inst)
+    finally:
+        file.close()
+
+    return render_template("index.html", count=count)
 
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
